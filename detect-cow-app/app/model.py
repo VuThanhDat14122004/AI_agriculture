671950cb32dd1bcac20c93937147c2fbfb65c876
Vu Thanh Dat <22022620@vnu.edu.vn>
2025-05-07 23:20:39 +0700

    add save

--- detect-cow-app/app/model.py
+++ detect-cow-app/app/model.py
@@ -5,6 +5,12 @@ import ultralytics
 
 
 torch.serialization.add_safe_globals([ultralytics.nn.tasks.DetectionModel])
+torch.serialization.add_safe_globals([torch.nn.modules.container.Sequential])
+torch.serialization.add_safe_globals([torch.nn.modules.module.Module])
+
+## add safe globals for YOLO from ultralytics
+torch.serialization.add_safe_globals([ultralytics.yolo.engine.model.YOLO])
+torch.serialization.add_safe_globals([ultralytics.yolo.engine.model.YOLOPredictor])
 
 model = YOLO("best.pt")
 
