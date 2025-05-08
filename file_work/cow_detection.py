3d0a6e4b479d964faf834f4770645b9303e49f9f
Vu Thanh Dat <22022620@vnu.edu.vn>
2025-05-08 22:12:33 +0700

    refactor python file

--- file_work/cow_detection.py
+++ file_work/cow_detection.py
@@ -19,17 +19,16 @@ while video.isOpened():
     count = len(results[0].boxes)
     cv2.putText(frame, f"number of cow: {count}", (30,30), font, 1, (0,255,0), 2, cv2.LINE_4)
     # Display the results
-    for result in results:
-        boxes = results[0].boxes.xyxy.cpu().numpy()  # Get bounding box coordinates
-        confidences = results[0].boxes.conf.cpu().numpy() # cpu to avoid cuda error 
-        for i in range(len(boxes)):
-            box = boxes[i]
-            x1, y1, x2, y2 = box  # x_min, ymin, xmax, ymax = box
-            confidence = confidences[i]
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-            cv2.putText(frame, f'cow, confidence: {confidence:.2f}',\
-                        (int(x1), int(y1) - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    boxes = results[0].boxes.xyxy.cpu().numpy()  # Get bounding box coordinates
+    confidences = results[0].boxes.conf.cpu().numpy() # cpu to avoid cuda error 
+    for i in range(len(boxes)):
+        box = boxes[i]                                                                                                                                                                                                                                                                                                                                                                                                  
+        x1, y1, x2, y2 = box  # x_min, ymin, xmax, ymax = box
+        confidence = confidences[i]
+        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+        cv2.putText(frame, f'cow, confidence: {confidence:.2f}',\
+                    (int(x1), int(y1) - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
